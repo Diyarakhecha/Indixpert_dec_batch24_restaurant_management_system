1a5096dbe674fa42c6e34c0cb832122d50ef5f99
Diyarakhecha <Diyarakhecha28@gmail.com>
2025-05-20 14:02:06 +0530

    changes

--- SRC/Authentication/Manage_profile.py
+++ SRC/Authentication/Manage_profile.py
@@ -1,22 +1,17 @@
 import json
 import datetime
 
-from SRC.Authentication.Sign_in import get_signin
-from SRC.Authentication.Sign_up import get_signup
-
-log=fr"C:\Indixpert_dec_batch24_restaurant_management_system\SRC\logs\logs.txt"
+from SRC.Authentication.Sign_in import signin
+from SRC.Authentication.Sign_up import signup
+from SRC.Authentication.Writelogs import writelogs
 
 class AuthenticationManager:
     def __init__(self):
       pass
 
-    def writelogs(self,logs):
-        with open(log, "a") as file:
-            file.write(logs + "\n")
-
     def display_menu(self):
             while True:
-                print("\n------ Authentication Menu -----")
+                print("\n------Wellcome to Rajasthani Thali -----")
                 print("1. Sign in")
                 print("2. Sign up")
                 print("3. Exit")
@@ -24,11 +19,11 @@ class AuthenticationManager:
                 try:
                     userchoice = int(input("Choose an option: "))
                     if userchoice == 1:
-                       object = get_signin()
-                       object.signin()
+                       object = signin()
+                       object.get_signin("admin")
                     elif userchoice == 2:
-                        object = get_signup()
-                        object.signup()
+                        object = signup()
+                        object.get_signup("staff")
                     elif userchoice == 3:
                         print("Goodbye!")
                         break
@@ -38,4 +33,4 @@ class AuthenticationManager:
                     print("\nInvalid input! please enter a number.")
                     data={"error":str(e) ,"date":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
                     logs=json.dumps(data,indent=4)
-                    self.writelogs(logs)
+                    writelogs(logs)
